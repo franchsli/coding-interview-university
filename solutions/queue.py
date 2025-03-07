967b27e625968d4c0b6cd64baaa644d5181aa733
franchsli <112600427+franchsli@users.noreply.github.com>
2025-03-07 17:06:20 -0500

    Studied

--- solutions/queue.py
+++ solutions/queue.py
@@ -15,18 +15,38 @@ class Queue(LinkedList):
     
 # fixed-size array implementation
 
-@dataclass
+
 class Queue:
-    items = [None] * 100
+    def __init__(self):
+        self.items = [None] * 100
 
     def enqueue(self, value):
-        self.push_front(value)
-    
-    def deque(self):
-        self.pop_back()
-    
-    def empty(self):
-        return self.is_empty()
+        if not self.is_full():
+            self.items = [value] + self.items[:len(self.items)-1]
+        else:
+            raise IndexError("The queue is full.")
 
-    def full(self):
-        pass
+    def get_first_in_index(self) -> int:
+        if self.is_full():
+            return len(self.items) - 1
+        
+        elif self.is_empty():
+            raise IndexError("The queue is empty.")
+        
+        for i in range(len(self.items)):
+            if self.items[i+1] == None:
+                return i
+
+
+    def deque(self):
+        if not self.is_empty():
+            first_in_index = self.get_first_in_index()
+            temp = self.items[first_in_index]
+            self.items[first_in_index] = None
+            return temp
+    
+    def is_empty(self):
+        return self.items[0] == None
+
+    def is_full(self):
+        return self.items[-1] != None
